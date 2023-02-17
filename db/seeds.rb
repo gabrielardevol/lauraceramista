4c0de487f32286d197e3f710372554ef4b0e26ce
Gabriel Ardevol <artsdevol@gmail.com>
2023-02-17 14:57:02 +0100

    yay, progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,8 +22,21 @@ Project.create([
   homepage_images: dir_to_array("app/assets/images/1/tr", "1/tr/"),
   showpage_images: dir_to_array('app/assets/images/1/op', '1/op/')},
 
-  {name: "Projecte2",
-  content:"Lorem ipsum dolor sit amet",
+  {name: "Juego-de-Café-o-Té-4uds",
+  content:"Llena tus mañanas de color y calidez con un diseño natural y funcional.
+  Puedes elegir entre 3 medidas diferentes: Café espresso, café cortado o té.
+  Cada set contiene 4 tazas.
+
+  Dimensiones:
+  • Taza Café Espresso: Diámetro 6cm / Altura 8,3cm
+  • Taza Café Cortado: Diámetro 7cm / Altura 9,3cm
+  • Taza Té: Diámetro 8,5cm / Altura 11,3cm
+
+  Material: Gres esmaltado.
+  Al tratarse de una pieza artesanal y gracias a nuestro esmaltado manual, cada taza es única y puede presentar pequeñas variaciones de tonalidad.
+
+  Fabricado artesanalmente en nuestro taller de La Bisbal d'Empordà (Gerona).
+  Apto para lavavajillas, microondas y horno.",
   homepage_images: dir_to_array("app/assets/images/2/tr", "2/tr/"),
   showpage_images: dir_to_array("app/assets/images/2/op", "2/op/"),
   },
@@ -47,5 +60,61 @@ Project.create([
   content:"Lorem ipsum dolor sit amet",
   homepage_images: dir_to_array("app/assets/images/5/tr", "5/tr/"),
   showpage_images: dir_to_array("app/assets/images/5/op", "5/op/")
+  },
+
+
+  {name: "Projecte6",
+    content:"Lorem ipsum dolor sit amet",
+    homepage_images: dir_to_array("app/assets/images/6/tr", "6/tr/"),
+    showpage_images: dir_to_array("app/assets/images/6/op", "6/op/")
+  },
+
+
+  {name: "Projecte7",
+    content:"Lorem ipsum dolor sit amet",
+    homepage_images: dir_to_array("app/assets/images/7/tr", "7/tr/"),
+    showpage_images: dir_to_array("app/assets/images/7/op", "7/op/")
+  },
+
+
+  {name: "Projecte8",
+    content:"Lorem ipsum dolor sit amet",
+    homepage_images: dir_to_array("app/assets/images/8/tr", "8/tr/"),
+    showpage_images: dir_to_array("app/assets/images/8/op", "8/op/")
+  },
+
+
+  {name: "Projecte9",
+    content:"Lorem ipsum dolor sit amet",
+    homepage_images: dir_to_array("app/assets/images/9/tr", "9/tr/"),
+    showpage_images: dir_to_array("app/assets/images/9/op", "9/op/")
+  },
+
+
+  {name: "Projecte10",
+    content:"Lorem ipsum dolor sit amet",
+    homepage_images: dir_to_array("app/assets/images/10/tr", "10/tr/"),
+    showpage_images: dir_to_array("app/assets/images/10/op", "10/op/")
+  },
+
+
+  {name: "Projecte11",
+    content:"Lorem ipsum dolor sit amet",
+    homepage_images: dir_to_array("app/assets/images/11/tr", "11/tr/"),
+    showpage_images: dir_to_array("app/assets/images/11/op", "11/op/")
+  },
+
+
+  {name: "Projecte12",
+    content:"Lorem ipsum dolor sit amet",
+    homepage_images: dir_to_array("app/assets/images/12/tr", "12/tr/"),
+    showpage_images: dir_to_array("app/assets/images/12/op", "12/op/")
+  },
+
+
+  {name: "Projecte13",
+    content:"Lorem ipsum dolor sit amet",
+    homepage_images: dir_to_array("app/assets/images/13/tr", "13/tr/"),
+    showpage_images: dir_to_array("app/assets/images/13/op", "13/op/")
   }
 ])
